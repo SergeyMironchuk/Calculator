8b6b813d11dcfaa52ad4ff055d576794910bf680
Prn Service <admin@prn-service.com>
2015-08-27 18:04:07 +0300

    Вычисление формулы из стека

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -10,10 +10,23 @@ import Foundation
 
 class CalculatorBrain
 {
-    private enum Op {
+    private enum Op: Printable {
         case Operand(Double)
         case UnaryOperation(String, Double -> Double)
         case BinaryOperation(String, (Double, Double) -> Double)
+        
+        var description: String {
+            get {
+                switch self {
+                case .Operand(let operand):
+                    return "\(operand)"
+                case .UnaryOperation(let symbol, _):
+                    return symbol
+                case .BinaryOperation(let symbol, _):
+                    return symbol
+                }
+            }
+        }
     }
     private var opStack = Array<Op>()
     
@@ -66,6 +79,44 @@ class CalculatorBrain
     
     func evaluate() -> Double? {
         let (result, remainder) = evaluate(opStack)
+        if let stack = displayStack() {
+            println("formula: \(stack)")
+        }
+        if (result != nil) {
+            println("stack: \(opStack) = \(result!) with \(remainder) left over")
+        } else {
+            println("stack: \(opStack) = \(result) with \(remainder) left over")
+        }
+        return result
+    }
+    
+    private func displayStack(ops: [Op]) -> (result: String?, remainingOpts: [Op]) {
+        if !ops.isEmpty {
+            var remainingOps = ops
+            let op = remainingOps.removeLast()
+            switch op {
+            case .Operand(let operand):
+                return ("\(operand)", remainingOps)
+            case .UnaryOperation(let operationSymbol, _):
+                let displayResult = displayStack(remainingOps)
+                if let operand = displayResult.result {
+                    return ("\(operationSymbol)(\(operand))", displayResult.remainingOpts)
+                }
+            case .BinaryOperation(let operationSymbol, _):
+                let displayResult1 = displayStack(remainingOps)
+                if let operand1 = displayResult1.result {
+                    let displayResult2 = displayStack(displayResult1.remainingOpts)
+                    if let operand2 = displayResult2.result {
+                        return ("(\(operand1)\(operationSymbol)\(operand2))", displayResult2.remainingOpts)
+                    }
+                }
+            }
+        }
+        return (nil, ops)
+    }
+    
+    func displayStack() -> String? {
+        let (result, remainder) = displayStack(opStack)
         return result
     }
 }
